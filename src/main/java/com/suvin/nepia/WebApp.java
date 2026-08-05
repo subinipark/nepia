6a3c19643b199ac43de99568879595ea24537c37
suviny <tnqls3718@gmail.com>
2026-08-06 01:20:54 +0900

    refactor: gitignore 파일 수정 및 클래스 및 파일 확장자 변경

--- src/main/java/com/suvin/nepia/WebApp.java
+++ src/main/java/com/suvin/nepia/WebApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NepiaApplication {
+public class WebApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NepiaApplication.class, args);
+		SpringApplication.run(WebApp.class, args);
 	}
 
 }

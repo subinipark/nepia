2e49614e3605544e80b41aedab8c63991bf9e319
suviny <tnqls3718@gmail.com>
2026-08-05 23:00:00 +0900

    최초 커밋

---
+++ src/test/java/com/suvin/nepia/NepiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suvin.nepia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NepiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
